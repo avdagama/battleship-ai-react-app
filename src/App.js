8165f4a7dce926ca20039d1bf9696295dac68b6c
Alex Dagama <47162944+avdagama@users.noreply.github.com>
2021-11-23 16:59:52 -0500

    comments

--- src/App.js
+++ src/App.js
@@ -282,30 +282,28 @@ function App() {
    * @param {string} resultingCellState The resulting state of the cell at userBoard[y][x]
    */
   function updateProbabilities(x, y, resultingCellState) {
-    //since we don't want to make this move again, set the probability to 0
-    // probabilityBoard[y][x] = 0.0;
 
     switch (resultingCellState) {
       //miss
       case 'M':
-         // TODO: update the probability board after a miss
+         //update the probability board after a miss
          probabilityBoard[y][x] = -1.0;
         break;
 
       //hit
       case 'H':
-        // TODO: update the probability board after a hit
+        //update the probability board after a hit
         probabilityBoard[y][x] = 0.0;
 
         //update horizontal and vertical as long as is on bounds 
         updateHV(y,x); 
 
-        let oritentation = lineDetector(y,x); 
-        //edgue decteo can return both side. 
-        //if edge increate the problabiity of both edgues by a bunch 
-        if(oritentation)
+        let orientation = lineDetector(y,x); 
+        
+        //if an edge is detected, update the probability for the edge
+        if(orientation)
         {
-          updateEdge(y,x,oritentation); 
+          updateEdge(y,x,orientation); 
         }
 
         break;
@@ -315,9 +313,14 @@ function App() {
     }
   }
   
-
   const maxArrayIndex = 10; 
   const minArrayIndex = 0; 
+  /**
+   * Checks if the coordinates are in the range of the board
+   * @param {int} x - the x coordinate
+   * @param {int} y - the y coordinate
+   * @returns A boolean representing if the coordinates are in range
+   */
   function inRange(y , x){
       if( minArrayIndex <= x && x < maxArrayIndex){
         if( minArrayIndex <= y && y < maxArrayIndex){
@@ -326,6 +329,12 @@ function App() {
       }
   }
 
+  /**
+   * Checks if we need to update the probability
+   * @param {int} x - the x coordinate
+   * @param {int} y - the y coordinate
+   * @returns 
+   */
   function discarted(y,x){
     //return (probabilityBoard[y][x] == 0) ? true : false
     if( probabilityBoard[y][x] === 0 )
@@ -344,6 +353,11 @@ function App() {
     return false; 
   }
   
+  /**
+   * Updates the proababilties around the coordinates in a tapered cross
+   * @param {int} x - the x coordinate
+   * @param {int} y - the y coordinate
+   */
   function updateHV(y,x){
     //Vertical 
     const percentage = .80 ; 
@@ -370,6 +384,12 @@ function App() {
     }
   }
 
+  /**
+   * Detects the affected ship's (if it exists) orientation on the last move
+   * @param {int} x - the x coordinate
+   * @param {int} y - the y coordinate
+   * @returns The orienation of the ship or null if no ship was detected
+   */
   function lineDetector(y,x){
     //check if up down left right was a hit. bef 
     if(  (inRange(y+1,x) &&  probabilityBoard[y+1][x] === 0) || (inRange(y-1,x) &&  probabilityBoard[y-1][x] === 0) )
@@ -379,6 +399,12 @@ function App() {
     return null; 
   }
 
+  /**
+   * Updates the probabilties if a ship is detected
+   * @param {int} x - the x coordinate
+   * @param {int} y - the y coordinate
+   * @param {*} orientation - the orientation of the detected ship, if one exists
+   */
   function updateEdge(y,x,orientation)
   {
     let percentage = 3.0; 
@@ -386,18 +412,18 @@ function App() {
     {
       console.log("Orientation updating ",{orientation})
       let right = x; 
-      //update left and righ t
+      //update left and right
       while(inRange(y,right) && probabilityBoard[y][right] === 0.0)
       {
         right ++; 
       }
-      //if in range updat epropb
+      //if in range update prob
       if(inRange(y,right))
         probabilityBoard[y][right] += probabilityBoard[y][right] * percentage
       
       //Left
       let left = x; 
-      //update left and righ t
+      //update left and right
       while(inRange(y,left) && probabilityBoard[y][left] === 0.0)
       {
         left --; 
@@ -409,7 +435,7 @@ function App() {
     else if(orientation === 'V')
     {
       let up = y; 
-      //update left and righ t
+      //update left and right
       while(inRange(up,x) && probabilityBoard[up][x] === 0.0)
       {
         up ++; 
@@ -420,7 +446,7 @@ function App() {
 
       //Down 
       let down = y; 
-      //update left and righ t
+      //update left and right
       while(inRange(down,x) && probabilityBoard[down][x] === 0.0)
       {
         down --; 
